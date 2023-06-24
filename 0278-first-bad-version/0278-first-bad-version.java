ea45486394daf073d809cc2a6d44e70b07eae3a2
shubham mani tripathi <42939900+mani9k9@users.noreply.github.com>
2023-06-24 17:59:55 +0530

    Time: 16 ms (96.87%), Space: 39.6 MB (41.58%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Solution extends VersionControl {
+    // APPROACH : 1
+    public int firstBadVersion(int n) {
+        int left = 1, right = n;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (isBadVersion(mid) == false) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+
+    // APPROACH : 2
+
+   /* public int firstBadVersion(int n) {
+        long left = 1, right = n;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+
+            if (isBadVersion((int) mid) == false) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return (int) left;
+    } */
+
+}
