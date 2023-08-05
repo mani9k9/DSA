69cf86c4b96f1718158244ad7c5427e061dee08f
shubham mani tripathi <42939900+mani9k9@users.noreply.github.com>
2023-08-05 13:59:46 +0530

    Time: 6 ms (25.34%), Space: 44 MB (85.38%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,21 +1,25 @@
 class Solution {
     public int missingNumber(int[] nums) {
+      //sort the array
         Arrays.sort(nums);
+        
+        //initialize the left, right and mid point
         int left = 0;
         int right = nums.length-1;
         
         while(left<=right){
-            int mid = (left+right)/2;
-        
-        if(nums[mid] == mid){
-            left = mid+1;
+            int mid= (left+right)/2;
+            
+            if(nums[mid]==mid){
+                left = left+1;
+            }
+            else if(nums[mid] > mid){
+                right = right-1;
+            }
+            else{
+                left=left+1;
+            }
         }
-        else if(nums[mid] > mid){
-            right = mid-1;
-        }
-        else{
-            left = mid+1;
-        }}
         return left;
     }
 }
