decc5bcba0f4103cb4950267d0fd4c43f7c2ac8f
shubham mani tripathi <42939900+mani9k9@users.noreply.github.com>
2023-08-09 13:22:14 +0530

    Time: 16 ms (99.38%), Space: 59 MB (88.75%) - LeetHub

--- 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.java
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.java
@@ -1,29 +1,28 @@
 class Solution {
     public int minimizeMax(int[] nums, int p) {
         
-  int n =nums.length;
+        int n = nums.length;
         Arrays.sort(nums);
-        int left =0;
-        int right =nums[n-1]-nums[0];
+        int left = 0;
+        int right = nums[n-1]-nums[0]; //find the maximum difference in array
         
         while(left<right){
             int mid = (left+right)/2;
-            int count = 0; //counter
+            int count=0; //counter
             
-            for(int  i = 0; i<n-1; i++){
-                if(nums[i+1] - nums[i] <= mid){
-                    i++;
-                    count++;
+            for(int i=0;i<n-1;i++){
+                if(nums[i+1]-nums[i]<=mid){
+                    i++; // move to next element, pair not having same index
+                    count++; //track pairs and increament the count
                 }
             }
             if(count>=p){
-                right =mid; //upper bound
+                right = mid; //upper bound
             }
             else{
-                left =mid+1; //lower bound
+                left = mid+1; //lower bound
             }
         }
-            return left;
-        
+        return left;
     }
 }
