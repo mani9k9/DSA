dd9e5c2195eb7c8f1ba026b1d038defe996913ff
shubham mani tripathi <42939900+mani9k9@users.noreply.github.com>
2023-08-09 13:09:32 +0530

    Time: 16 ms (99.38%), Space: 59 MB (77.50%) - LeetHub

--- 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.java
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.java
@@ -1,34 +1,29 @@
 class Solution {
     public int minimizeMax(int[] nums, int p) {
         
-        int n = nums.length;
-Arrays.sort(nums);
-int left = 0;
-int right = nums[n - 1] - nums[0];  // maximum difference in array
-
-while (left < right) {
-    int mid = (left + right) / 2;
-    int c = 0; // Counter to keep track of pairs formed
-    
-    // Loop through the array to find pairs with a difference less than or equal to mid
-    for (int i = 0; i < n - 1; i++) {
-        if (nums[i + 1] - nums[i] <= mid) {
-            i++; // Move to the next element as pairs can't have the same index
-            c++; // Increment the counter to track the pairs formed
+  int n =nums.length;
+        Arrays.sort(nums);
+        int left =0;
+        int right =nums[n-1]-nums[0];
+        
+        while(left<right){
+            int mid = (left+right)/2;
+            int count = 0; //counter
+            
+            for(int  i = 0; i<n-1; i++){
+                if(nums[i+1] - nums[i] <= mid){
+                    i++;
+                    count++;
+                }
+            }
+            if(count>=p){
+                right =mid; //upper bound
+            }
+            else{
+                left =mid+1; //lower bound
+            }
         }
-    }
-    
-    // Check if the number of pairs formed is greater than or equal to p
-    if (c >= p) {
-        right = mid; // Adjust the upper bound of possible differences
-    } else {
-        left = mid + 1; // Adjust the lower bound of possible differences
-    }
-}
-
-// The binary search ends when left and right converge, which will be the minimum maximum difference
-return left;
-
+            return left;
         
     }
 }
