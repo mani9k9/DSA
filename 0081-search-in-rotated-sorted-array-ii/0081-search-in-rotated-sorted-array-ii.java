41bf8f67bffeb35ac060675db2f3fbcecf383299
shubham mani tripathi <42939900+mani9k9@users.noreply.github.com>
2023-08-10 13:27:05 +0530

    Time: 0 ms (100.00%), Space: 43 MB (73.97%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,30 +1,33 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
+      int left = 0;
+        int right = nums.length-1;
+        
+        while(left<=right){
+            int mid = left+ (right-left)/2;
+            
+            if(nums[mid]== target){
                 return true;
             }
-
-            if (nums[mid] == nums[left] && nums[mid] == nums[right]) {
+            
+            if(nums[mid]==nums[left] && nums[mid]==nums[right]){
                 left++;
                 right--;
-            } else if (nums[mid] >= nums[left]) {
-                if (target >= nums[left] && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            } else if (target <= nums[right] && target > nums[mid]) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
             }
-        }
+            else if(nums[mid]>=nums[left]){
+                if(target >=nums[left] && target<nums[mid]){
+                    right=mid-1;
+                }else{
+                    left=mid+1;
+                }
+            }
+                else if(target<=nums[right] && target> nums[mid]){
+                    left=mid+1;
+                }else{
+                    right=mid-1;
+                }
+            }
+        
         return false;
     }
 }
